a157810f3c5d52a7104c79e59a0994669a1d2e7c
cq <cq18250703562@163.com>
2021-07-30 14:22:07 +0800

    1

---
+++ pages/category/category.vue
@@ -0,0 +1,93 @@
+<<template>
+	<view class="content">
+		<scroll-view scroll-with-animation scroll-y class="right-aside">
+			<view class="s-list">
+				<view class="t-list">
+					<view @click="navToList(item.id)" class="t-item" v-for="item in cateList">
+						<text>{{item.name}}1</text>
+					</view>
+				</view>
+			</view>
+		</scroll-view>
+	</view>
+</template>
+
+
+
+<script>
+	export default {
+		data() {
+			return {
+				sizeCalcState: false,
+				tabScrollTop: 0,
+				currentId: 1,
+				cateList: [],
+			}
+		},
+		onLoad(){
+			this.loadData();
+		},
+		methods: {
+			async loadData(){
+				let cateList = await this.$api.json('cateList');
+				this.cateList = cateList || [];
+			},
+			navToList(sid){
+				uni.navigateTo({
+					url: `/pages/product/list?sid=${sid}`
+				})
+			}
+		}
+	}
+</script>
+
+<style lang='scss'>
+	page,
+	.content {
+		height: 100%;
+		background-color: #f8f8f8;
+	}
+
+	.content {
+		display: flex;
+	}
+	.right-aside{
+		flex: 1;
+		overflow: hidden;
+	}
+	.s-item{
+		display: flex;
+		align-items: center;
+		height: 70upx;
+		padding-top: 8upx;
+		font-size: 28upx;
+		color: $font-color-dark;
+	}
+	.t-list{
+		display: flex;
+		flex-wrap: wrap;
+		width: 100%;
+		background: #fff;
+		padding-top: 12upx;
+		&:after{
+			content: '';
+			flex: 99;
+			height: 0;
+		}
+	}
+	.t-item{
+		flex-shrink: 0;
+		display: flex;
+		justify-content: center;
+		align-items: left;
+		flex-direction: column;
+		width: 345upx;
+		margin-top:20upx;
+		margin-left:20upx;
+		font-size: 26upx;
+		color: #666;
+		background:#eee;
+		padding: 20upx 0 20upx 30upx;
+		border-radius: 10upx;
+	}
+</style>
---
+++ pages/imgdow/imgdow.vue
@@ -0,0 +1,227 @@
+<template>
+	<view class="container">
+		<view class="detail-desc">
+			<view >
+				<img :src="item.src" class="item-r" alt=""  @click="choosed(item,index)" :key="index" v-for="(item,index) in imgList" ref='liId' style="float: left;width:23.75%;margin-top:1%;margin-left:1%;"  >
+			</view>
+		</view>
+		<!-- 底部操作菜单 -->
+		<view class="page-bottom">
+			<view class="p-b-btn" style="background-color: #F0AD4E;" @click="downAll()">
+				<text>下载并复制相册描述</text>
+			</view>
+			<view class="p-b-btn" style="background-color: #DD524D;" @click="imgDow()">
+				<text>一键下载</text>
+			</view>
+		</view>
+	</view>
+</template>
+<script>
+	export default{
+		components: {
+		},
+		data() {
+			return {
+				checkBox: [],    // 选中的内容
+				imgList: [
+					{
+						id:1,
+						src: 'https://gd3.alicdn.com/imgextra/i3/0/O1CN01IiyFQI1UGShoFKt1O_!!0-item_pic.jpg_400x400.jpg'
+					},
+					{
+						id:2,
+						src: 'https://gd3.alicdn.com/imgextra/i3/TB1RPFPPFXXXXcNXpXXXXXXXXXX_!!0-item_pic.jpg_400x400.jpg'
+					},
+					{
+						id:3,
+						src: 'https://gd2.alicdn.com/imgextra/i2/38832490/O1CN01IYq7gu1UGShvbEFnd_!!38832490.jpg_400x400.jpg'
+					},
+					{
+						id:4,
+						src: 'https://gd3.alicdn.com/imgextra/i3/0/O1CN01IiyFQI1UGShoFKt1O_!!0-item_pic.jpg_400x400.jpg'
+					},
+					{
+						id:5,
+						src: 'https://gd3.alicdn.com/imgextra/i3/TB1RPFPPFXXXXcNXpXXXXXXXXXX_!!0-item_pic.jpg_400x400.jpg'
+					},
+					{
+						id:6,
+						src: 'https://gd2.alicdn.com/imgextra/i2/38832490/O1CN01IYq7gu1UGShvbEFnd_!!38832490.jpg_400x400.jpg'
+					}
+				],
+			};
+		},
+		async onLoad(options){
+		},
+		methods:{
+			// 多选
+			choosed(item,index) {
+				var idx = this.checkBox.indexOf(index);
+				console.log(this)
+				this.$nextTick(function(){
+					if(this.$refs.liId[index].className == 'item-r') {
+						// 添加类--选中状态
+						this.$refs.liId[index].className = 'item-r choose';
+						this.checkBox.push(item);
+					} else {
+						// 选中再取消
+						this.$refs.liId[index].className = 'item-r';
+						this.checkBox.splice(idx, 1);
+					}
+				})
+			},
+			//图片下载
+			imgDow(){
+				this.checkBox.forEach(function(e) {
+					uni.downloadFile({
+						url: e.src,
+						success: (res) => {
+							if (res.statusCode === 200) {
+								uni.saveImageToPhotosAlbum({
+									filePath: res.tempFilePath,
+									success: (res) => {
+										console.log('保存成功');
+									},
+									fail: () => console.log('保存失败')
+								 })
+								console.log('下载成功');
+							}
+						}
+					 });
+				})
+			},
+			//图片下载并复制文本
+			downAll(){
+				uni.setClipboardData({
+				    data: 'hello',
+				    success: function () {
+				        console.log('success');
+				    }
+				});
+				this.checkBox.forEach(function(e) {
+					uni.downloadFile({
+						url: e.src,
+						success: (res) => {
+							if (res.statusCode === 200) {
+								uni.saveImageToPhotosAlbum({
+									filePath: res.tempFilePath,
+									success: (res) => {
+										console.log('保存成功');
+									},
+									fail: () => console.log('保存失败')
+								 })
+								console.log('下载成功');
+							}
+						}
+					 });
+				})
+			},
+		},
+	}
+</script>
+
+<style lang='scss'>
+	page{
+		background: $page-color-base;
+		padding-bottom: 160upx;
+	}
+	/*  详情 */
+	.detail-desc{
+		background: #fff;
+		margin-top: 32upx;
+		clear: both;
+		.choose{
+			filter: alpha(opacity=20);
+			opacity: 0.2;
+			position: relative;
+			&.choose::after {
+			 content: '✔';
+			 display: block;
+			 height: 0px;
+			 width: 0px;
+			 position: absolute;
+			 right: 0;
+			 bottom: 0;
+			 color:#fff;
+			 /**对号大小*/
+			 font-size: 20upx;
+			 line-height: 16upx;
+			 border: 20upx solid;
+			 border-color: transparent #4884ff #4884ff transparent;
+		   }
+		}
+	}
+	
+	
+	/* 底部操作菜单 */
+	.page-bottom{
+		position:fixed;
+		bottom:0;
+		z-index: 95;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		height: 100upx;
+		background: rgba(255,255,255,.9);
+		
+		.p-b-btn{
+			display:flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			font-size: $font-sm;
+			color: #fff;
+			width: 500upx;
+			height: 100upx;
+			.yticon{
+				font-size: 40upx;
+				line-height: 48upx;
+				color: $font-color-light;
+			}
+			&.active, &.active .yticon{
+				color: $uni-color-primary;
+			}
+			.icon-fenxiang2{
+				font-size: 42upx;
+				transform: translateY(-2upx);
+			}
+			.icon-shoucang{
+				font-size: 46upx;
+			}
+		}
+		.action-btn-group{
+			display: flex;
+			height: 76upx;
+			border-radius: 100px;
+			overflow: hidden;
+			box-shadow: 0 20upx 40upx -16upx #fa436a;
+			box-shadow: 1px 2px 5px rgba(219, 63, 96, 0.4);
+			background: linear-gradient(to right, #ffac30,#fa436a,#F56C6C);
+			margin-left: 20upx;
+			position:relative;
+			&:after{
+				content: '';
+				position:absolute;
+				top: 50%;
+				right: 50%;
+				transform: translateY(-50%);
+				height: 28upx;
+				width: 0;
+				border-right: 1px solid rgba(255,255,255,.5);
+			}
+			.action-btn{
+				display:flex;
+				align-items: center;
+				justify-content: center;
+				width: 180upx;
+				height: 100%;
+				font-size: $font-base ;
+				padding: 0;
+				border-radius: 0;
+				background: transparent;
+			}
+		}
+	}
+	
+</style>
